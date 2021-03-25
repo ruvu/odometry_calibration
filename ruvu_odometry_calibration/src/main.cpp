94ed1a23b90286c4019967e1286ed466f7a4021f
Ramon Wijnands <ramon@ruvu.nl>
2021-03-25 20:45:22 +0100

    Implement reverse odometry

--- ruvu_odometry_calibration/src/main.cpp
+++ ruvu_odometry_calibration/src/main.cpp
@@ -6,6 +6,8 @@
 #include <tf2_msgs/TFMessage.h>
 #include <tf2/LinearMath/Transform.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/utils.h>
+#include <angles/angles.h>
 
 ros::Duration duration_from_bag(const rosbag::Bag& bag)
 {
@@ -43,6 +45,19 @@ public:
   }
 };
 
+struct Odometry
+{
+  double linear;
+  double angular;
+};
+
+Odometry inverse_odometry(const tf2::Stamped<tf2::Transform>& a, const tf2::Stamped<tf2::Transform>& b)
+{
+  auto angular = angles::shortest_angular_distance(tf2::getYaw(a.getRotation()), tf2::getYaw(b.getRotation()));
+  auto linear = (b.getOrigin() - a.getOrigin()).length();
+  return Odometry{ linear, angular };
+}
+
 int main(int argc, char** argv)
 {
   if (argc != 2)
@@ -55,31 +70,57 @@ int main(int argc, char** argv)
   rosbag::Bag bag;
   bag.open(argv[1], rosbag::bagmode::Read);
 
+  // load a tf buffer with all transforms
   BagBuffer buffer(bag);
 
+  // load all measurements from the bagfile
+  std::vector<tf2::Stamped<tf2::Transform>> measurements;
+  for (const auto& msg : rosbag::View(bag, rosbag::TopicQuery("/tf")))
   {
-    for (const auto& msg : rosbag::View(bag, rosbag::TopicQuery("/scan")))
+    auto tfs = msg.instantiate<tf2_msgs::TFMessage>();
+    if (tfs == nullptr)
+      throw std::runtime_error("Received tf message with the wrong type");
+
+    for (const auto& tf : tfs->transforms)
     {
-      auto scan = msg.instantiate<sensor_msgs::LaserScan>();
-      if (scan == nullptr)
-        throw std::runtime_error("Received scan message with the wrong type");
-
-      tf2::Stamped<tf2::Transform> measurement;
-      try
+      if (tf.header.frame_id == "odom")
       {
-        tf2::fromMsg(buffer.lookupTransform("odom", "base_link", scan->header.stamp), measurement);
-      }
-      catch (const tf2::ExtrapolationException&)
-      {
-        ROS_WARN("skipping measurment because of unknown transform");
-      }
+        if (tf.child_frame_id != "base_link")
+          throw std::runtime_error("Received odom tf with the wrong child_frame_id");
 
-      auto p = measurement.getOrigin();
-      std::cout << p.getX() << " " << p.getY() << " " << p.getZ() << '\n';
+        tf2::Stamped<tf2::Transform> measurement;
+        tf2::fromMsg(tf, measurement);
+        auto p = measurement.getOrigin();
+        measurements.push_back(measurement);
+      }
     }
   }
 
   bag.close();
+
+  // iterate in pairs over the measurements
+  auto a = measurements.begin();
+  auto b = a + 1;
+  for (; b != measurements.end(); a = b++)
+  {
+    auto odometry = inverse_odometry(*a, *b);
+    std::cout << odometry.linear << " " << odometry.angular << '\n';
+  }
+
+  // TODO
+
+  //      tf2::Stamped<tf2::Transform> measurement;
+  //      try
+  //      {
+  //        tf2::fromMsg(buffer.lookupTransform("odom", "base_link", scan->header.stamp), measurement);
+  //      }
+  //      catch (const tf2::ExtrapolationException&)
+  //      {
+  //        ROS_WARN("skipping measurment because of unknown transform");
+  //      }
+
+  //      auto p = measurement.getOrigin();
+  //      std::cout << p.getX() << " " << p.getY() << " " << p.getZ() << '\n';
 
   return 0;
 }
