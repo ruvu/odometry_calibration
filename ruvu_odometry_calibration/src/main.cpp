3be37eb400bfec0eec1e2c48606d091efc5e6aa8
Ramon Wijnands <ramon@ruvu.nl>
2021-03-25 17:50:56 +0100

    Work on loading the measurements

--- ruvu_odometry_calibration/src/main.cpp
+++ ruvu_odometry_calibration/src/main.cpp
@@ -4,6 +4,8 @@
 #include <sensor_msgs/LaserScan.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_msgs/TFMessage.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 ros::Duration duration_from_bag(const rosbag::Bag& bag)
 {
@@ -16,14 +18,14 @@ ros::Duration duration_from_bag(const rosbag::Bag& bag)
     ts.push_back(scan->header.stamp);
   }
 
-  printf("Loaded %zu scans", ts.size());
+  ROS_INFO("Loaded %zu scans", ts.size());
   if (!ts.size())
     throw std::runtime_error("Can't find any scans in the bagfile");
 
   return ts.back() - ts.front();
 }
 
-class BagBuffer : tf2_ros::Buffer
+class BagBuffer : public tf2_ros::Buffer
 {
 public:
   BagBuffer(const rosbag::Bag& bag) : Buffer(duration_from_bag(bag))
@@ -45,15 +47,37 @@ int main(int argc, char** argv)
 {
   if (argc != 2)
   {
-    printf("usage: %s BAGFILE", argv[0]);
+    ROS_INFO("usage: %s BAGFILE", argv[0]);
     return 1;
   }
 
-  printf("loading %s", argv[1]);
+  ROS_INFO("loading %s", argv[1]);
   rosbag::Bag bag;
   bag.open(argv[1], rosbag::bagmode::Read);
 
   BagBuffer buffer(bag);
+
+  {
+    for (const auto& msg : rosbag::View(bag, rosbag::TopicQuery("/scan")))
+    {
+      auto scan = msg.instantiate<sensor_msgs::LaserScan>();
+      if (scan == nullptr)
+        throw std::runtime_error("Received scan message with the wrong type");
+
+      tf2::Stamped<tf2::Transform> measurement;
+      try
+      {
+        tf2::fromMsg(buffer.lookupTransform("odom", "base_link", scan->header.stamp), measurement);
+      }
+      catch (const tf2::ExtrapolationException&)
+      {
+        ROS_WARN("skipping measurment because of unknown transform");
+      }
+
+      auto p = measurement.getOrigin();
+      std::cout << p.getX() << " " << p.getY() << " " << p.getZ() << '\n';
+    }
+  }
 
   bag.close();
 
